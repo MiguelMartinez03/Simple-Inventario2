517486e341b68736e7aa256c633a2c42075cef62
A3 <mm22-1094@unphu.edu.do>
2024-01-24 21:16:59 -0400

    Tarea

---
+++ SimpleAgenda.java
@@ -0,0 +1,14 @@
+public class SimpleAgenda {
+    public static void agregarcontacto (String a){
+    }
+    public static void removercontacto (int b){
+    }
+
+    public static String actualizarcontacto(int M){
+
+        return "N/A";
+    }
+
+    public static void main(String[] args) {
+    }
+}
